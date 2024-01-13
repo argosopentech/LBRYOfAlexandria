8415cdd23151190badb098a27302fd798a3f5526
P.J. Finlay <pjappdevelopment@gmail.com>
2024-01-13 10:22:36 -0600

    Tweaks

--- lbryofalex.py
+++ lbryofalex.py
@@ -16,6 +16,8 @@ import pathlib
 import lbrytools as lbryt
 from lbrytools import download_single
 
+P2P_DISCLAIMER = """LBRY is a peer-to-peer network. All content is contributed by users and LBRY does not censor or moderate the content of the network. Any conent you download or seed can be associated with your public IP address."""
+
 
 class LBRYOfAlexandria(QMainWindow):
     def __init__(self):
@@ -27,9 +29,6 @@ class LBRYOfAlexandria(QMainWindow):
 
         # Create a SearchWidget for LBRY claims search
         self.search_widget = SearchWidget(self)
-        self.search_widget.list_widget.itemClicked.connect(
-            self.search_widget.showClaimDetails
-        )
 
         # Create a DownloadWidget for LBRY URI download
         self.download_widget = DownloadWidget(self)
@@ -47,6 +46,47 @@ class LBRYOfAlexandria(QMainWindow):
         self.setWindowTitle("LBRY of Alexandria")
 
 
+class SearchWidget(QWidget):
+    LIST_ITEM_URI_ROLE = 1
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.edit_search = QLineEdit(self)
+        self.edit_search.returnPressed.connect(self.searchClaims)
+
+        btn_search = QPushButton("Search", self)
+        btn_search.clicked.connect(self.searchClaims)
+
+        self.list_widget = QListWidget(self)
+
+        # Set up the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit_search)
+        layout.addWidget(btn_search)
+        layout.addWidget(self.list_widget)
+        self.setLayout(layout)
+
+    def searchClaims(self):
+        search_text = self.edit_search.text()
+        results = lbryt.list_search_claims(text=search_text)
+        self.list_widget.clear()
+        claims = results.get("claims", [{}])
+        claims.sort(key=lambda claim: float(claim.get("amount", "0")), reverse=True)
+        for claim in claims:
+            canonical_url = claim.get("canonical_url")
+            if canonical_url is None:
+                continue
+            item_text = canonical_url
+            list_item = QListWidgetItem(item_text)
+            list_item.setData(self.LIST_ITEM_URI_ROLE, claim["canonical_url"])
+            list_item.setFlags(list_item.flags() | 2)  # Make item selectable
+            self.list_widget.addItem(list_item)
+
+
 class DownloadWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -54,7 +94,7 @@ class DownloadWidget(QWidget):
         self.initUI()
 
     def initUI(self):
-        label = QLabel("Download URI", self)
+        label = QLabel("Download URI:", self)
 
         # Create a QLineEdit for URI input
         self.edit_uri = QLineEdit(self)
@@ -74,62 +114,11 @@ class DownloadWidget(QWidget):
         uri = self.edit_uri.text()
         ddir = str(pathlib.Path.home() / "Documents" / "LBRYOfAlexandria")
         pathlib.Path(ddir).mkdir(parents=True, exist_ok=True)
-        own_dir = True
         # Includes: download_path, channel_name, channel_claim_id, claim_name, content_fee
         #          download_directory, download_path, metadata, title, streaming_url
-        result = lbryt.download_single(uri, ddir, own_dir)
-        QMessageBox.information(self, "Download Result", f"Download result: {result}")
-
-
-class SearchWidget(QWidget):
-    LIST_ITEM_URI_ROLE = 1
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.parent = parent
-        self.initUI()
-
-    def initUI(self):
-        self.edit_search = QLineEdit(self)
-        self.edit_search.returnPressed.connect(self.searchClaims)
-
-        btn_search = QPushButton("Search", self)
-        btn_search.clicked.connect(self.searchClaims)
-
-        self.list_widget = QListWidget(self)
-        self.list_widget.itemClicked.connect(self.showClaimDetails)
-
-        self.claim_details_label = QLabel("Claim Details", self)
-
-        # Set up the layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.edit_search)
-        layout.addWidget(btn_search)
-        layout.addWidget(self.list_widget)
-        layout.addWidget(self.claim_details_label)
-        self.setLayout(layout)
-
-    def searchClaims(self):
-        search_text = self.edit_search.text()
-        results = lbryt.list_search_claims(text=search_text)
-        self.list_widget.clear()
-        claims = results.get("claims", [{}])
-        claims.sort(key=lambda claim: float(claim.get("amount", "0")), reverse=True)
-        for claim in claims:
-            amount = float(claim.get("amount", "0"))
-            canonical_url = claim.get("canonical_url")
-            if canonical_url is None:
-                continue
-            item_text = f"({amount:.2f}) {canonical_url}"
-            list_item = QListWidgetItem(item_text)
-            list_item.setData(self.LIST_ITEM_URI_ROLE, claim["canonical_url"])
-            list_item.setFlags(list_item.flags() | 2)  # Make item selectable
-            self.list_widget.addItem(list_item)
-
-    def showClaimDetails(self, item):
-        uri = item.data(self.LIST_ITEM_URI_ROLE)
-        if uri:
-            self.claim_details_label.setText(uri)
+        result = lbryt.download_single(uri, ddir=ddir, own_dir=True)
+        print(result)
+        QMessageBox.information(self, "Downloading", P2P_DISCLAIMER)
 
 
 if __name__ == "__main__":
